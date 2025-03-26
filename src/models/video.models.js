1812e6a8cba15c1b02a6e3f1276d669c295d1e0a
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2025-03-26 12:19:03 +0500

    Pipeline aggregation

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new mongoose.Schema(
     {
@@ -47,5 +48,6 @@ const videoSchema = new mongoose.Schema(
         }
 )
 
+mongoose.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
