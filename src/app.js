feeef362e72d9fddb1e450aa84848745adabf1f5
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2025-03-30 16:04:06 +0500

    added user routes

--- src/app.js
+++ src/app.js
@@ -5,10 +5,18 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({ origin: process.env.CORS_ORIGIN }))
-app.use(express.json({ limit: '15kb' }))   // the limit applied on the data sending to server
-app.use(express.urlencoded({ extended: true, limit: '15kb' }))
+
+app.use(express.json({ limit: '25kb' }))   // the limit applied on the data sending to server
+app.use(express.urlencoded({ extended: true, limit: '25kb' }))
 app.use(express.static("public"))
+
 app.use(cookieParser()) // allows to perform CRUD operation on user's cookies
 
+
+// import routes
+import userRouter from "./routes/user.routes.js"
+
+// routes
+app.use("/user", userRouter)
 
 export { app }
