f4de9605a585086fe4419229b726221785750343
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2025-04-02 14:46:05 +0500

    Subscription schema

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema(
     {
+        // both channels and subscriber contains the user ID
         channel: {
             type: mongoose.Schema.Types.ObjectId, // the one who owned the channel
             ref: "User"
