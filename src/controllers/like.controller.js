dda808db0ed2722e4130409411c23da60ecc7b8b
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2025-04-03 23:59:28 +0500

    Implement like functionality and added endpoint to retrieve liked videos

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,25 +1,155 @@
 import mongoose from "mongoose"
+import asyncHandler from '../utils/asyncHandler.js'
+import ApiError from "../utils/ApiError.js"
+import ApiResponse from "../utils/ApiResponse.js";
+import { Like } from "../models/like.models.js"
 
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
+    const { videoId } = req.params
+
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId");
+    }
+
+    const deleted = await Like.findOneAndDelete({
+        video: videoId,
+        likedBy: req.user?._id
+    })
+
+    if (deleted) {
+        return res.status(204).json(
+            new ApiResponse(204, {}, "Unliked Video!")
+        )
+    }
+
+    await Like.create({
+        video: videoId,
+        likedBy: req.user?._id
+    })
+
+    return res.status(201).json(
+        new ApiResponse(201, {}, "Liked Video!")
+    )
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+    const { commentId } = req.params
 
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid commentId");
+    }
+
+    const deleted = await Like.findOneAndDelete({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    if (deleted) {
+        return res.status(204).json(
+            new ApiResponse(204, {}, "Unliked Comment!")
+        )
+    }
+
+    await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    return res.status(201).json(
+        new ApiResponse(201, {}, "Liked Comment!")
+    )
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+    const { tweetId } = req.params
 
+    if (!mongoose.isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweetId");
+    }
+
+    const deleted = await Like.findOneAndDelete({
+        tweet: tweetId,
+        likedBy: req.user?._id
+    })
+
+    if (deleted) {
+        return res.status(204).json(
+            new ApiResponse(204, {}, "Unliked Tweet!")
+        )
+    }
+
+    await Like.create({
+        tweet: tweetId,
+        likedBy: req.user?._id
+    })
+
+    return res.status(201).json(
+        new ApiResponse(201, {}, "Liked Tweet!")
+    )
+})
+
+//TODO: get all liked videos
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: req.user?._id
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "video",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        userName: 1,
+                                        fullName: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    },
+                    {
+                        $project: {
+                            title: 1,
+                            description: 1,
+                            views: 1,
+                            owner: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $project: {
+                likedBy: 1,
+                video: 1
+            }
+        }
+    ])
+
+    return res.status(200).json(
+        new ApiResponse(200, likedVideos, "All Liked videos are fetched!")
+    )
 })
 
 export {
