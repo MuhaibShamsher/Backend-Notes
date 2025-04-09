38898ddc7d81209c07f2f16ddc302231bf9166fb
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2025-04-10 02:13:13 +0500

    Added subscription routes with JWT verification

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import verifyJWT from "../middlewares/auth.middleware.js";
+import { 
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+} from "../controllers/subscription.controller.js"
+
+
+const router = Router()
+router.use(verifyJWT)
+
+
+router.route("/channel/:channelID")
+    .patch(toggleSubscription)
+    .get(getUserChannelSubscribers)
+
+router.route("/user")
+    .get(getSubscribedChannels)
+
+    
+export default router
